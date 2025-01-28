5e1235abd5ed79df4f3226ea7b0675da45710695
Minji Ardanuy-Jetté <minjiajette@gmail.com>
2025-01-28 11:00:34 -0500

    Compléter un exemple d'instances de plusieurs cubes

--- js/cube.js
+++ js/cube.js
@@ -1,7 +1,6 @@
 import * as THREE from "three";
-// import { OrbitControls } from "three/addons/controls/OrbitControls";
-// import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
 
+//Scène et caméra
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -9,20 +8,60 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+
+//Rendu
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
+renderer.setAnimationLoop(animate); // Animation de l'ancienne version du cube vert fluo
 document.body.appendChild(renderer.domElement);
 
+//Géométrie, mesh, matériel
+//Cube vert fluo
 const geometrie = new THREE.BoxGeometry(1, 1, 1);
-const materiel = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const materiel = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometrie, materiel);
-scene.add(cube);
+// scene.add(cube);
+
+//Fonction pour faire des instances
+function creerInstances(geometrie, color, x) {
+  const materiel = new THREE.MeshPhongMaterial({ color });
+  const cube = new THREE.Mesh(geometrie, materiel);
+  scene.add(cube);
+  cube.position.x = x;
+  return cube;
+}
+//Listes des cubes
+const cubes = [
+  creerInstances(geometrie, 0x44aa88, 0),
+  creerInstances(geometrie, 0x8844aa, -2),
+  creerInstances(geometrie, 0xaa8844, 2),
+];
 
 camera.position.z = 5;
 
+//Lumière dirrectionnelle
+const color = 0xffffff;
+const intensity = 3;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
+//Fonction pour animer le cube
 function animate() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
   renderer.render(scene, camera);
+}
+
+function render(temps) {
+  temps *= 0.001; // convertir en secondes le temps
+  cubes.forEach((cube, nombre) => {
+    const vitesse = 1 + nombre * 0.1;
+    const rotation = temps + vitesse;
+    cube.rotation.x = rotation;
+    cube.rotation.y = rotation;
+  });
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(render);
 }
